f92df87a1b2398a506b59010fea3fa3c9b2d1e9e
maarlesh <subamaarleshbaskar@gmail.com>
2023-09-01 20:24:58 +0530

    Created Paddle and ball

--- main.lua
+++ main.lua
@@ -10,10 +10,11 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 --setting virtual height and widht -- 
-VIRTUAL_WIDTH = 600
-VIRTUAL_HEIGHT = 400
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 function love.load()
+    --love.graphics.setDefaultFilter('nearest', 'nearest')
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -34,7 +35,20 @@ end
 function love.draw()
     --start
     push:apply('start')
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+
+    --applying color
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    --game title bar
+    love.graphics.printf('Hello Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
+    --paddle1
+    love.graphics.rectangle('fill',5,30,5,20)
+    --paddle2
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-40,5,20)
+    --ball
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4)
+    
+    
     --end
     push:apply('end')
 end
